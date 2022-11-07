260a6b4b953a06c5bb5c9806080f6aca572e361b
Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>
2022-11-07 02:13:25 +0100

    tailwind added
    Signed-off-by: Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,7 +1,20 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
+import React, { useState, useRef } from 'react'
+
 export default function Home() {
+  const url = useRef<HTMLInputElement>(null)
+  const [video, setVideo] = useState('')
+
+  const handleVideo = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault()
+    console.log(url.current?.value)
+
+    if (url.current) 
+      setVideo(encodeURIComponent(url.current.value))
+  }
+
   return (
     <div>
       <Head>
@@ -9,6 +22,18 @@ export default function Home() {
         <meta name="description" content="Video proxy client" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <main>
+        <div className='flex'>
+          <input type="text" placeholder="Enter video URL" ref={url}/>
+          <button onClick={handleVideo}>Submit</button>
+        </div>
+        <div className='flex'>
+          <video controls autoPlay>
+            <source src={`http://localhost:3001/video/${video}`} type="video/mp4" />
+          </video>
+        </div>
+      </main>
     </div>
   )
 }
