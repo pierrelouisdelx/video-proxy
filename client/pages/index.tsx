852a3b05a1620e396a1b5f107c73ade9e5f37238
Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>
2022-11-07 01:54:49 +0100

    next client with tailwind init
    Signed-off-by: Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>

---
+++ client/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Video Proxy</title>
+        <meta name="description" content="Video proxy client" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
